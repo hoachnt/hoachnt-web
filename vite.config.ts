821f3eea7dbe1b2f2b89b0cf8e476be8a8c8ece9
hoach-linux <hoachprogrammer@gmail.com>
2023-01-19 12:40:20 +0700

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,38 @@
 import { fileURLToPath, URL } from "node:url";
-
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Nguyen Tien Hoach",
+        short_name: "NAPP",
+        description: "Website about Nguyen Tien Hoach",
+        theme_color: "#2563eb",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
