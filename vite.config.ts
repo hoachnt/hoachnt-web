b181ecf204e2957a11900ab79e242adafc65c0d8
hoach-linux <hoachprogrammer@gmail.com>
2023-01-19 13:00:24 +0700

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,13 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      injectRegister: "auto",
       registerType: "autoUpdate",
+      injectRegister: "auto",
       devOptions: {
         enabled: true,
       },
       workbox: {
+        cleanupOutdatedCaches: true,
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
@@ -22,6 +23,8 @@ export default defineConfig({
         short_name: "Nguyen",
         description: "Website about Nguyen Tien Hoach",
         theme_color: "#2563eb",
+        background_color: "#2563eb",
+        display: "standalone",
         icons: [
           {
             src: "pwa-192x192.png",
