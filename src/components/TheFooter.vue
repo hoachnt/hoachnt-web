7289b351e0f6e19d4b8a9404b4c75775ab4b17fd
hoach-linux <hoachprogrammer@gmail.com>
2023-01-20 10:25:36 +0700

    optimized website's speed

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,6 +2,7 @@
   <div class="min-w-screen flex justify-center items-center py-24">
     <a
       href="https://www.facebook.com/hoachprogrammer"
+      aria-label="facebook"
       v-ripple
       class="p-ripple"
     >
@@ -10,7 +11,12 @@
         class="text-3xl text-black mx-4 hover:text-blue-600 transition duration-300"
       />
     </a>
-    <a href="https://github.com/hoach-linux" v-ripple class="p-ripple">
+    <a
+      href="https://github.com/hoach-linux"
+      aria-label="github"
+      v-ripple
+      class="p-ripple"
+    >
       <font-awesome-icon
         class="text-3xl text-black mx-4 hover:text-blue-600 transition duration-300"
         icon="fa-brands fa-github"
