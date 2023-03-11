49ecd76d3052d55460c2c37859960de21a1e17ec
hoach-linux <hoachprogrammer@gmail.com>
2023-03-11 19:23:41 +0700

    unbundle chunksplit strategy

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     vue(),
     vuetify(),
     chunkSplitPlugin({
-      strategy: 'unbundle'
+      strategy: 'all-in-one'
     }),
     VitePWA({
       registerType: "autoUpdate",
