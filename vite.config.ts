7332653a7c8eb4868e87070988dbd831ff7c796a
hoach-linux <hoachprogrammer@gmail.com>
2023-03-11 17:41:46 +0700

    customized code

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 import vuetify from 'vite-plugin-vuetify'
+import { splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vuetify(),
+    splitVendorChunkPlugin(),
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "auto",
