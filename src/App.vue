ca1dccee3aaac3304f865ef031994b18746e5d7d
hoach-linux <hoachprogrammer@gmail.com>
2024-02-07 16:45:16 +0700

    redesigned

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import TheNavbar from "./components/TheNavbar.vue";
-import TheFooter from "./components/TheFooter.vue";
+import TheNavbar from "@/components/TheNavbar.vue";
+import TheFooter from "@/components/TheFooter.vue";
 </script>
 
 <template>
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,35 +1,48 @@
 <template>
-    <v-app-bar :elevation="0" class="border-b">
-        <template v-slot:prepend>
-            <v-app-bar-nav-icon class="md:hidden" @click.stop="visibleBottom = !visibleBottom" color="white">
-                <font-awesome-icon icon="fa-solid fa-bars" />
-            </v-app-bar-nav-icon>
-        </template>
-        <v-app-bar-title @click="$router.push('/')">Nguyen Tien Hoach</v-app-bar-title>
+  <v-app-bar :elevation="0" class="border-b">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon
+        class="md:hidden"
+        @click.stop="visibleBottom = !visibleBottom"
+        color="white"
+      >
+        <font-awesome-icon icon="fa-solid fa-bars" />
+      </v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title @click="$router.push('/')"
+      >Nguyen Tien Hoach</v-app-bar-title
+    >
 
-        <template v-slot:append>
-            <div class="hidden md:block">
-                <v-tabs color="blue" v-model="tab" fixed-tabs>
-                    <v-tab @click="$router.push(tabItem.value)" v-for="tabItem in tabs" :key="tabItem.value"
-                        :value="tabItem.value">{{ tabItem.text }}</v-tab>
-                    <v-tab @click="download">Resume</v-tab>
-                </v-tabs>
-            </div>
-            <v-app-bar-nav-icon href="https://blog.hoachnt.com/">
-                <v-icon>mdi-post</v-icon>
-                <v-tooltip activator="parent" location="bottom">My Blog</v-tooltip>
-            </v-app-bar-nav-icon>
-        </template>
-    </v-app-bar>
-    <v-navigation-drawer v-model="visibleBottom" location="bottom" temporary>
-        <v-list v-model="tab">
-            <v-list-subheader class="uppercase">{{ drawerHeader }}</v-list-subheader>
-            <v-divider></v-divider>
-            <v-list-item @click="$router.push(tabItem.value)" v-for="tabItem in tabs" :key="tabItem.value">{{ tabItem.text
-            }}</v-list-item>
-            <v-list-item @click="download">Resume</v-list-item>
-        </v-list>
-    </v-navigation-drawer>
+    <template v-slot:append>
+      <div class="hidden md:block">
+        <v-tabs color="blue" v-model="tab" fixed-tabs>
+          <v-tab
+            @click="$router.push(tabItem.value)"
+            v-for="tabItem in tabs"
+            :key="tabItem.value"
+            :value="tabItem.value"
+            >{{ tabItem.text }}</v-tab
+          >
+        </v-tabs>
+      </div>
+      <v-app-bar-nav-icon href="https://blog.hoachnt.com/">
+        <v-icon>mdi-post</v-icon>
+        <v-tooltip activator="parent" location="bottom">My Blog</v-tooltip>
+      </v-app-bar-nav-icon>
+    </template>
+  </v-app-bar>
+  <v-navigation-drawer v-model="visibleBottom" location="bottom" temporary>
+    <v-list v-model="tab">
+      <v-list-subheader class="uppercase">{{ drawerHeader }}</v-list-subheader>
+      <v-divider></v-divider>
+      <v-list-item
+        @click="$router.push(tabItem.value)"
+        v-for="tabItem in tabs"
+        :key="tabItem.value"
+        >{{ tabItem.text }}</v-list-item
+      >
+    </v-list>
+  </v-navigation-drawer>
 </template>
 <script setup lang="ts">
 import axios from "axios";
@@ -38,39 +51,23 @@ import { useRoute } from "vue-router";
 const visibleBottom = ref(false);
 const tab = ref(window.location.pathname);
 const tabs = ref([
-    { text: "Home", value: "/" },
-    { text: "Works", value: "/works" },
-    { text: "About", value: "/about" },
+  { text: "Home", value: "/" },
+  { text: "Works", value: "/works" },
+  { text: "About", value: "/about" },
 ]);
 const route = useRoute();
 const drawerHeader: any = ref("Nguyen Tien Hoach");
 
 watch(
-    () => route.path,
-    (currentTab: string) => {
-        tab.value = currentTab;
-        drawerHeader.value = route.name;
-    }
+  () => route.path,
+  (currentTab: string) => {
+    tab.value = currentTab;
+    drawerHeader.value = route.name;
+  }
 );
-
-const download = async () => {
-    let response = await axios({
-        url: "https://directus.hoach.skryonline.com/assets/8eab6914-528f-4384-95ae-370561da3158",
-        method: "GET",
-        responseType: "blob",
-    });
-    let fileURL = window.URL.createObjectURL(new Blob([response.data]));
-    let fileLink = document.createElement("a");
-
-    fileLink.href = fileURL;
-    fileLink.setAttribute("download", "Nguyen-Tien-Hoach_Resume.pdf");
-    document.body.appendChild(fileLink);
-
-    fileLink.click();
-};
 </script>
 <style>
 .bottom-bar {
-    min-height: 300px;
+  min-height: 300px;
 }
 </style>
