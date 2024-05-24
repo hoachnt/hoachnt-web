9caa730204c55a59c78b2dabd602dd214300f6f2
hoachnt <hoachprogrammer@gmail.com>
2024-05-24 18:46:04 +0700

    Translate lab page

--- pages/lab.vue
+++ pages/lab.vue
@@ -2,14 +2,8 @@
   <main class="min-h-screen">
     <AppHeader class="mb-12" :title="$t('lab.lab')" :description="$t('lab.description')" />
     <div class="space-y-24">
-      <ContentList path="/lab" v-slot="{ list }">
-        <ContentQuery
-          v-for="item in list"
-          :key="item._path"
-          :path="item._path"
-          find="one"
-          v-slot="{ data }"
-        >
+      <ContentList :path="localePath('/lab')" v-slot="{ list }">
+        <ContentQuery v-for=" item  in  list " :key="item._path" :path="item._path" find="one" v-slot="{ data }">
           <ContentRenderer>
             <ContentRendererMarkdown :value="data" />
           </ContentRenderer>
@@ -20,6 +14,7 @@
 </template>
 
 <script setup>
+const localePath = useLocalePath()
 const description = "Some random experiments with UI I find in my free time.";
 useSeoMeta({
   title: "Lab | Nguyen Tien Hoach",
