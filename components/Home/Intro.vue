741e28558c6b72fc47462e031954a32a3abafd2c
hoachnt <hoachprogrammer@gmail.com>
2024-05-29 14:18:51 +0700

    added avatar

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="space-y-6">
-    <h1 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100 capitalize">
-      {{ $t('home.hello') }}.
+    <NuxtImg
+      src="/avatar.jpg"
+      alt="Hoachnt"
+      class="ring-2 border ring-gray-200 border-gray-300 dark:ring-white/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-12 w-12 sm:h-16 sm:w-16"
+      sizes="48px sm:64px"
+      placeholder
+      format="webp"
+    />
+    <h1
+      class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100 capitalize"
+    >
+      {{ $t("home.hello") }}.
     </h1>
     <p class="text-gray-900 dark:text-gray-400">
-      {{ $t('home.aboutMeText1') }}
+      {{ $t("home.aboutMeText1") }}
     </p>
     <p class="text-gray-900 dark:text-gray-400">
-      {{ $t('home.aboutMeText2') }}
+      {{ $t("home.aboutMeText2") }}
     </p>
   </div>
 </template>
