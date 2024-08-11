68e2d21d9261c0e098389939ea87809301726281
hoachnt <hoachprogrammer@gmail.com>
2024-08-11 20:29:29 +0700

    Redesign navbar

--- components/App/Navbar.vue
+++ components/App/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
   <div
     ref="headerRef"
-    :style="[styles]"
-    class="nav-wrapper fixed top-0 w-full z-50 overflow-x-auto overflow-y-hidden bg-inherit backdrop-blur-md"
+    :style="styles"
+    class="nav-wrapper fixed top-0 w-full z-50 bg-inherit backdrop-blur-md"
   >
-    <nav class="mx-auto px-4 sm:px-6 lg:px-8 min-w-[397px] max-w-2xl">
+    <nav class="mx-auto px-3 max-w-2xl">
       <ul
-        class="nav-list flex items-center my-9 px-3 text-sm font-medium text-gray-800 rounded-full shadow-lg bg-white/90 shadow-gray-800/5 ring-1 backdrop-blur dark:bg-gray-800/90 dark:text-gray-200 dark:ring-white/20 ring-gray-900/5"
+        class="flex items-center h-[52px] my-4 px-3 text-sm font-medium text-gray-800 dark:text-gray-200"
       >
         <li
           v-for="item in items"
           :key="item.path"
-          class="nav-item"
           v-cursor-block
+          class="hidden lg:block"
         >
           <UTooltip
             :text="item.name"
@@ -29,10 +29,7 @@
                 :name="$route.path === item.path ? item.iconActive : item.icon"
                 class="w-5 h-5 z-10"
               />
-              <span
-                v-if="$route.path === item.path"
-                class="active-line absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-primary-500/0 via-primary-500/70 to-primary-500/0 dark:from-primary-400/0 dark:via-primary-400/40 dark:to-primary-400/0"
-              ></span>
+
               <span
                 v-if="$route.path === item.path"
                 class="active-bg absolute h-8 w-8 z-0 rounded-full bg-gray-500/10 dark:bg-white/10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
@@ -41,6 +38,20 @@
             </ULink>
           </UTooltip>
         </li>
+
+        <UButton
+          color="primary"
+          variant="outline"
+          size="sm"
+          class="lg:hidden z-10 block"
+          v-cursor-block
+          @click="isOpen = true"
+        >
+          Menu
+        </UButton>
+
+        <AppNavigationMenu :close="() => close()" v-model:is-open="isOpen" />
+
         <li class="nav-item flex-1"></li>
         <li class="nav-item ml-3" v-cursor-block>
           <AppLanguageSelect />
@@ -72,6 +83,7 @@ const localePath = useLocalePath();
 const { t } = useI18n();
 const headerRef = ref(null);
 const { styles } = useFixedHeader(headerRef);
+const isOpen = ref(false);
 
 const items = computed(() => {
   return [
@@ -107,6 +119,10 @@ const items = computed(() => {
     },
   ];
 });
+
+function close() {
+  isOpen.value = false;
+}
 </script>
 
 <style scoped>
---
+++ components/App/NavigationMenu.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+defineProps<{ close(): void }>();
+const isOpen = defineModel<boolean>("isOpen");
+
+const localePath = useLocalePath();
+const { t } = useI18n();
+
+const navlist = computed(() => [
+  { path: localePath("/"), name: t("navbar.home") },
+  {
+    name: t("navbar.projects"),
+    path: localePath("/projects"),
+  },
+  {
+    name: t("navbar.articles"),
+    path: localePath("/articles"),
+  },
+  {
+    name: t("navbar.bookmarks"),
+    path: localePath("/bookmarks"),
+  },
+  {
+    name: t("navbar.lab"),
+    path: localePath("/lab"),
+  },
+]);
+</script>
+
+<template>
+  <div>
+    <USlideover v-model="isOpen" side="right">
+      <div class="p-4 flex justify-center items-center min-h-full flex-col">
+        <UButton
+          color="gray"
+          variant="ghost"
+          size="sm"
+          icon="i-heroicons-x-mark-20-solid"
+          class="flex lg:hidden absolute end-5 top-5 z-10"
+          square
+          padded
+          @click="close()"
+        />
+        <ULink
+          v-for="(item, index) in navlist"
+          :key="item.path"
+          :to="item.path"
+          active-class="text-primary"
+          class="text-4xl py-4 duration-200 transition ease-in-out w-full text-center"
+          inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+          @click="close()"
+        >
+          {{ item.name }}
+        </ULink>
+      </div>
+    </USlideover>
+  </div>
+</template>
