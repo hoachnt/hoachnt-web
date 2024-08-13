024caa65216b180a828a35213a273cfc9872b67e
hoachnt <hoachprogrammer@gmail.com>
2024-08-13 16:21:54 +0700

    Improve SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,106 +1,109 @@
 export default defineNuxtConfig({
-  experimental: {
-    // viewTransition: true,
-  },
-  devtools: { enabled: true },
-  routeRules: {
-    "/bookmarks": { prerender: true },
-  },
-  // nitro: {
-  //   prerender: {
-  //     crawlLinks: true,
-  //   },
-  // },
+    experimental: {
+        // viewTransition: true,
+    },
+    devtools: { enabled: true },
+    routeRules: {
+        "/bookmarks": { prerender: true },
+    },
+    nitro: {
+      prerender: {
+        crawlLinks: true,
+      },
+    },
 
-  plugins: ["~/plugins/ipad-cursor"],
+    plugins: ["~/plugins/ipad-cursor"],
 
-  modules: [
-    "@nuxt/ui",
-    "nuxt-icon",
-    "@nuxtjs/google-fonts",
-    "@nuxtjs/fontaine",
-    "@nuxt/image",
-    "@nuxt/content",
-    "@nuxthq/studio",
-    "@vueuse/nuxt",
-    "@nuxtjs/i18n",
-    "@vueuse/nuxt",
-    "nuxt-delay-hydration",
-    "@nuxtjs/sitemap",
-  ],
-  site: {
-    url: "https://www.hoachnt.com",
-    name: "Nguyen Tien Hoach",
-  },
-  delayHydration: {
-    mode: "mount",
-    replayClick: true,
-    // enables nuxt-delay-hydration in dev mode for testing
-    // NOTE: you should disable this once you've finished testing, it will break HMR
-    debug: process.env.NODE_ENV === "development",
-  },
-  i18n: {
-    vueI18n: "./i18n.config.ts",
-    skipSettingLocaleOnNavigate: true,
-    defaultLocale: "en",
-    langDir: "lang",
-    strategy: "prefix_except_default",
-    locales: [
-      {
-        name: "English",
-        value: "en",
-        code: "en",
-        file: "en.ts",
-      },
-      {
-        name: "Русский",
-        value: "ru",
-        code: "ru",
-        file: "ru.ts",
-      },
-      {
-        name: "Tiếng việt",
-        value: "vn",
-        code: "vn",
-        file: "vn.ts",
-      },
+    modules: [
+        "@nuxt/ui",
+        "nuxt-icon",
+        "@nuxtjs/google-fonts",
+        "@nuxtjs/fontaine",
+        "@nuxt/image",
+        "@nuxt/content",
+        "@nuxthq/studio",
+        "@vueuse/nuxt",
+        "@nuxtjs/i18n",
+        "@vueuse/nuxt",
+        "nuxt-delay-hydration",
+        "@nuxtjs/sitemap",
     ],
-    detectBrowserLanguage: {
-      useCookie: true,
-      cookieKey: "i18n_redirected",
-      redirectOn: "root", // recommended
-      alwaysRedirect: true,
+
+    site: {
+        url: "https://www.hoachnt.com",
+        name: "Nguyen Tien Hoach",
     },
-  },
-  ui: {
-    icons: ["heroicons", "lucide"],
-  },
-  app: {
-    pageTransition: { name: "page", mode: "out-in" },
-    head: {
-      htmlAttrs: {
-        lang: "en",
-        class: "h-full",
-      },
-      bodyAttrs: {
-        class: "antialiased bg-gray-50 dark:bg-black min-h-screen",
-      },
+    delayHydration: {
+        mode: "mount",
+        replayClick: true,
+        // enables nuxt-delay-hydration in dev mode for testing
+        // NOTE: you should disable this once you've finished testing, it will break HMR
+        debug: process.env.NODE_ENV === "development",
     },
-  },
-  content: {
-    highlight: {
-      theme: "github-dark",
+    i18n: {
+        vueI18n: "./i18n.config.ts",
+        skipSettingLocaleOnNavigate: true,
+        defaultLocale: "en",
+        langDir: "lang",
+        strategy: "prefix_except_default",
+        locales: [
+            {
+                name: "English",
+                value: "en",
+                code: "en",
+                file: "en.ts",
+            },
+            {
+                name: "Русский",
+                value: "ru",
+                code: "ru",
+                file: "ru.ts",
+            },
+            {
+                name: "Tiếng việt",
+                value: "vn",
+                code: "vn",
+                file: "vn.ts",
+            },
+        ],
+        detectBrowserLanguage: {
+            useCookie: true,
+            cookieKey: "i18n_redirected",
+            redirectOn: "root", // recommended
+            alwaysRedirect: true,
+        },
     },
-  },
-  googleFonts: {
-    display: "swap",
-    families: {
-      Inter: [400, 500, 600, 700, 800, 900],
-      Raleway: [400, 500, 600, 700, 800, 900],
+    ui: {
+        icons: ["heroicons", "lucide"],
     },
-  },
-  image: {
-    domains: ["hoachnt.com"],
-    format: ["webp"],
-  },
+    app: {
+        pageTransition: { name: "page", mode: "out-in" },
+        head: {
+            htmlAttrs: {
+                lang: "en",
+                class: "h-full",
+            },
+            bodyAttrs: {
+                class: "antialiased bg-gray-50 dark:bg-black min-h-screen",
+            },
+        },
+    },
+    content: {
+        highlight: {
+            theme: "github-dark",
+        },
+    },
+    googleFonts: {
+        display: "swap",
+        families: {
+            Inter: [400, 500, 600, 700, 800, 900],
+            Raleway: [400, 500, 600, 700, 800, 900],
+        },
+    },
+    image: {
+        domains: ["hoachnt.com"],
+        format: ["webp"],
+    },
+    build: {
+    },
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,69 +1,70 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
-module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-    "./app.vue",
-  ],
-  theme: {
-    extend: {
-      highlight: "#6d6d6d",
-      boxShadow: {
-        zoop: "rgba(102, 109, 128, 0.08) 0px 1.2672px 1.2672px 0px, rgba(102, 109, 128, 0.08) 0px 5.06879px 10.1376px 0px",
-        zoopdark:
-          "rgba(10, 10, 10, 0.2) 0px 1.2672px 1.2672px 0px, rgba(10, 10, 10, 0.2) 0px 5.06879px 10.1376px 0px",
-      },
-      colors: {
-        primary: {
-          "50": "#f6f6f6",
-          "100": "#e7e7e7",
-          "200": "#d1d1d1",
-          "300": "#b0b0b0",
-          "400": "#7c7c7c",
-          "500": "#6d6d6d",
-          "600": "#5d5d5d",
-          "700": "#4f4f4f",
-          "800": "#454545",
-          "900": "#3d3d3d",
-          "950": "#262626",
+export default <Partial<Config>>{
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+        "./app.vue",
+    ],
+    theme: {
+        extend: {
+            highlight: "#6d6d6d",
+            boxShadow: {
+                zoop: "rgba(102, 109, 128, 0.08) 0px 1.2672px 1.2672px 0px, rgba(102, 109, 128, 0.08) 0px 5.06879px 10.1376px 0px",
+                zoopdark:
+                    "rgba(10, 10, 10, 0.2) 0px 1.2672px 1.2672px 0px, rgba(10, 10, 10, 0.2) 0px 5.06879px 10.1376px 0px",
+            },
+            colors: {
+                primary: {
+                    50: "#f6f6f6",
+                    100: "#e7e7e7",
+                    200: "#d1d1d1",
+                    300: "#b0b0b0",
+                    400: "#7c7c7c",
+                    500: "#6d6d6d",
+                    600: "#5d5d5d",
+                    700: "#4f4f4f",
+                    800: "#454545",
+                    900: "#3d3d3d",
+                    950: "#262626",
+                },
+            },
         },
-      },
+        fontFamily: {
+            sans: [
+                "Raleway",
+                "Inter",
+                "Avenir Next",
+                "Roboto",
+                "-apple-system",
+                "BlinkMacSystemFont",
+                '"Segoe UI"',
+                "Ubuntu",
+                '"Helvetica Neue"',
+                "Arial",
+                '"Noto Sans"',
+                "sans-serif",
+                '"Apple Color Emoji"',
+                '"Segoe UI Emoji"',
+                '"Segoe UI Symbol"',
+                '"Noto Color Emoji"',
+            ],
+            mono: [
+                "Cascadia Code",
+                "ui-monospace",
+                "SFMono-Regular",
+                "Menlo",
+                "Monaco",
+                "Consolas",
+                "Liberation Mono",
+                "Courier New",
+                "monospace",
+            ],
+        },
     },
-    fontFamily: {
-      sans: [
-        "Raleway",
-        "Inter",
-        "Avenir Next",
-        "Roboto",
-        "-apple-system",
-        "BlinkMacSystemFont",
-        '"Segoe UI"',
-        "Ubuntu",
-        '"Helvetica Neue"',
-        "Arial",
-        '"Noto Sans"',
-        "sans-serif",
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ],
-      mono: [
-        "Cascadia Code",
-        "ui-monospace",
-        "SFMono-Regular",
-        "Menlo",
-        "Monaco",
-        "Consolas",
-        "Liberation Mono",
-        "Courier New",
-        "monospace",
-      ],
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
-} satisfies Config;
+    plugins: [require("@tailwindcss/typography")],
+};
