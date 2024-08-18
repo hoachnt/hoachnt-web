650994182a0b95b1129ac94816e4ffad7c4960b3
hoachnt <hoachprogrammer@gmail.com>
2024-08-18 15:58:20 +0700

    Update scroll to top time out

--- plugins/scroll-to-top.client.ts
+++ plugins/scroll-to-top.client.ts
@@ -2,8 +2,6 @@ import { defineNuxtPlugin } from "#app";
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.hook("page:finish", () => {
-        setTimeout(() => {
-            window.scrollTo(0, 0);
-        }, 300);
+        window.scrollTo(0, 0);
     });
 });
