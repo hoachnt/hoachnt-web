637a71c76d55d2749a8024e188116466d751faf2
hoachnt <hoachprogrammer@gmail.com>
2024-09-03 13:33:39 +0700

    Redesign ScrollToTop

--- components/App/ScrollToTop.vue
+++ components/App/ScrollToTop.vue
@@ -9,7 +9,7 @@ function scrollToTop() {
 <template>
     <UButton
         icon="i-heroicons-chevron-up"
-        variant="soft"
+        variant="outline"
         size="xl"
         color="black"
         class="fixed bottom-10 right-10 backdrop-blur-md bg-gray-200/50 text-black hover:!bg-gray-300 dark:bg-gray-800/50 dark:text-white dark:hover:!bg-gray-900 transition duration-200"
