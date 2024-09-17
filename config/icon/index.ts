b491fc04a23dcfbea09a1c6948871478567a99de
hoachnt <hoachprogrammer@gmail.com>
2024-09-17 19:00:30 +0700

    Update nuxt icon config

--- config/icon/index.ts
+++ config/icon/index.ts
@@ -3,5 +3,6 @@ import type { ModuleOptions } from "@nuxt/icon";
 export default <Partial<ModuleOptions>>{
     serverBundle: {
         collections: ["heroicons-solid", "heroicons", "mdi", "solar", "logos"], // <!--- this
+        externalizeIconsJson: true,
     },
 };
