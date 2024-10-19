0f3819e18d87e0dac732da2bd38574d0d38d7798
hoachnt <hoachprogrammer@gmail.com>
2024-10-20 01:16:46 +0700

    Revert "Add custom provider to nuxt image for fleek"
    This reverts commit e1891f4142ad11ea0735ab53c3ac345ee61391bb.

--- config/image/index.ts
+++ config/image/index.ts
@@ -3,14 +3,23 @@ import type { ModuleOptions } from "@nuxt/image";
 export default <Partial<ModuleOptions>>{
     domains: ["www.hoachnt.com"],
     format: ["webp"],
-    providers: {
-        myProvider: {
-            name: "myProvider", // optional value to overrider provider name
-            provider: "~/providers/my-provider.ts", // Path to custom provider
-            options: {
-                // ... provider options
-                baseURL: "https://hoachnt.on-fleek.app",
-            },
-        },
+    provider: "vercel",
+    vercel: {
+        baseURL: "https://www.hoachnt.com/_vercel/image",
+    },
+    screens: {
+        xs: 10,
+        sm: 40,
+        md: 48,
+        lg: 50,
+        xl: 60,
+        xxl: 64,
+        "2xl": 66,
+        "3xl": 96,
+        "4xl": 128,
+        "5xl": 132,
+        "6xl": 80,
+        "7xl": 600,
+        "8xl": 1200,
     },
 };
