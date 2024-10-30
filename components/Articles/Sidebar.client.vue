b042e06885306fe0ed7a0159771e13d9aad59b63
hoachnt <hoachprogrammer@gmail.com>
2024-10-30 23:25:08 +0700

    Update animation of ArticlesSidebar

--- components/Articles/Sidebar.client.vue
+++ components/Articles/Sidebar.client.vue
@@ -9,13 +9,14 @@ const isHide = ref(false);
 <template>
     <aside
         :class="[
-            'p-4 dark:bg-gray-800/50 bg-gray-200/50 backdrop-blur-3xl mb-11 sticky top-0 left-0 z-50 max-h-min',
+            'p-4 dark:bg-gray-800/50 bg-gray-200/50 backdrop-blur-3xl mb-11 sticky top-0 left-0 z-50 max-h-min transition-all duration-200',
+            isHide ? 'rounded-full top-1 mx-10 delay-200' : 'rounded-none',
         ]"
     >
         <ul
             :class="[
                 'space-y-2 transition-all duration-200',
-                isHide ? '!max-h-0' : 'max-h-52',
+                isHide ? '!max-h-0 px-10' : 'max-h-52 delay-200',
             ]"
         >
             <li v-for="section in sections" :key="section.id">
@@ -27,7 +28,7 @@ const isHide = ref(false);
             </li>
         </ul>
         <UButton
-            class="fixed bottom-0 left-1/2 translate-y-1/2 -translate-x-1/2"
+            class="fixed bottom-0 left-1/2 translate-y-1/2 -translate-x-1/2 transition"
             :icon="
                 !isHide ? 'i-heroicons-chevron-up' : 'i-heroicons-chevron-down'
             "
