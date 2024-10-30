f46503989c614ff374dea85b422ecff914fc86b7
hoachnt <hoachprogrammer@gmail.com>
2024-10-31 03:37:49 +0700

    Improve Sidebar animation

--- components/Articles/Sidebar.client.vue
+++ components/Articles/Sidebar.client.vue
@@ -3,14 +3,18 @@ defineProps<{
     sections: { id: string; title: string | null }[];
 }>();
 
-const isHide = ref(false);
+const isHide = ref(true);
+
+onMounted(() => setTimeout(() => (isHide.value = false), 600));
 </script>
 
 <template>
     <aside
         :class="[
-            'p-4 dark:bg-gray-800/80 bg-gray-200/80 backdrop-blur-3xl mb-11 sticky top-0 left-0 z-50 max-h-min transition-all duration-200',
-            isHide ? 'rounded-full top-1 mx-10 delay-200' : 'rounded-none',
+            'p-4 dark:bg-gray-800/80 bg-gray-200/80 backdrop-blur-3xl mb-11 sticky top-0 left-0 z-50 max-h-min mx-auto transition-all duration-200',
+            isHide
+                ? 'rounded-full top-1 delay-200 max-w-28'
+                : 'rounded-none max-w-full',
         ]"
     >
         <ul
@@ -27,16 +31,23 @@ const isHide = ref(false);
                 >
             </li>
         </ul>
-        <UButton
-            class="fixed bottom-0 left-1/2 translate-y-1/2 -translate-x-1/2 transition"
-            :icon="
-                !isHide ? 'i-heroicons-chevron-up' : 'i-heroicons-chevron-down'
-            "
-            :ui="{
-                rounded: 'rounded-full',
-            }"
+        <button
+            :class="[
+                'fixed bottom-0 left-1/2 -translate-x-1/2 flex justify-center items-center p-2 rounded-full transition-all duration-200 delay-300',
+                isHide
+                    ? 'w-full'
+                    : 'translate-y-1/2 w-8 text-white dark:text-black bg-primary delay-0',
+            ]"
             @click="isHide = !isHide"
-        />
+        >
+            <Icon
+                :name="
+                    !isHide
+                        ? 'i-heroicons-chevron-up'
+                        : 'i-heroicons-chevron-down'
+                "
+            />
+        </button>
     </aside>
 </template>
 
