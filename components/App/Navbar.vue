bb579ad9ad87272ff4c448333d98597564bf4bf1
hoachnt <hoachprogrammer@gmail.com>
2024-11-01 17:32:07 +0700

    Update navbar transition animation

--- components/App/Navbar.vue
+++ components/App/Navbar.vue
@@ -60,9 +60,15 @@ function close() {
                         :to="item.path"
                         prefetch
                         class="relative px-3 py-2 rounded-full flex items-center justify-center"
-                        active-class="text-white bg-black dark:text-black dark:bg-white"
+                        active-class="text-white dark:text-black"
                         :active="$route.path === item.path"
                     >
+                        <Transition>
+                            <span
+                                v-if="$route.path === item.path"
+                                class="active-bg absolute h-full w-full -z-10 bg-black dark:bg-white rounded-full"
+                            />
+                        </Transition>
                         {{ item.name }}
                     </ULink>
                 </li>
