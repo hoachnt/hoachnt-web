35be300d5d4a6c1db22196102af6cbe9eaea51a1
hoachnt <hoachprogrammer@gmail.com>
2024-11-03 04:52:26 +0700

    Improve rendering logic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
 
     nitro: {
         prerender: {
-            routes: ["/sitemap.xml", "/articles", "/projects", "/404"],
+            routes: ["/sitemap.xml", "/articles", "/projects"],
         },
     },
     routeRules: {
