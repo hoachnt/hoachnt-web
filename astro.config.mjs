6bbf2fc5f597ac74a99e065ae9f5c589dd349f6b
hoachnt <hoachprogrammer@gmail.com>
2025-03-23 01:23:51 +0700

    feat(tailwind): upgrade to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,48 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
-import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    output: 'static',
-    trailingSlash: 'always',
-    site: 'https://hoachnt.com',
+	output: "static",
+	trailingSlash: "always",
+	site: "https://hoachnt.com",
 
-    // Single page, no prefetch needed
-    prefetch: false,
+	// Single page, no prefetch needed
+	prefetch: false,
 
-    integrations: [
-        tailwind(),
-        sitemap(),
-        compress({
-            CSS: true,
-            SVG: false,
-            Image: false,
-            HTML: {
-                "html-minifier-terser": {
-                    collapseWhitespace: true,
-                    // collapseInlineTagWhitespace: true, // It breaks display-inline / flex-inline text
-                    minifyCSS: true,
-                    minifyJS: true,
-                    removeComments: true,
-                    removeEmptyAttributes: true,
-                    // removeEmptyElements: true, // It removes sometimes SVGs
-                    removeRedundantAttributes: true
-                },
-            },
-            JavaScript: {
-                'terser': {
-                    compress: {
-                        drop_console: true,
-                        drop_debugger: true,
-                    }
-                }
-            }
-        })
-    ]
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	integrations: [
+		sitemap(),
+		compress({
+			CSS: true,
+			SVG: false,
+			Image: false,
+			HTML: {
+				"html-minifier-terser": {
+					collapseWhitespace: true,
+					// collapseInlineTagWhitespace: true, // It breaks display-inline / flex-inline text
+					minifyCSS: true,
+					minifyJS: true,
+					removeComments: true,
+					removeEmptyAttributes: true,
+					// removeEmptyElements: true, // It removes sometimes SVGs
+					removeRedundantAttributes: true,
+				},
+			},
+			JavaScript: {
+				terser: {
+					compress: {
+						drop_console: true,
+						drop_debugger: true,
+					},
+				},
+			},
+		}),
+	],
 });
