e1bbafc6fb32a922b739e57fd6e7c0dbaec003b0
hoachnt <hoachprogrammer@gmail.com>
2025-03-25 16:59:46 +0700

    chore: update 'About Me' description

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,7 +9,7 @@ export const ui = {
 	en: {
 		"profile.name": "Nguyen Tien Hoach",
 		"profile.description":
-			"I am a developer with expertise in backend, frontend, and DevOps. I focus on creating innovative IT solutions and exploring new approaches in programming and technology.",
+			"I am a developer with expertise in backend, frontend, and DevOps.",
 		"contact.initializing": "Initializing...",
 		"contact.ready": "Ready to connect.",
 		"contact.waiting": "Waiting for contact request...",
@@ -17,7 +17,7 @@ export const ui = {
 	vn: {
 		"profile.name": "Nguyễn Tiến Hoạch",
 		"profile.description":
-			"Tôi là một nhà phát triển có chuyên môn về backend, frontend và DevOps. Tôi tập trung vào việc tạo ra các giải pháp CNTT sáng tạo và khám phá các phương pháp tiếp cận mới trong lập trình và công nghệ.",
+			"Tôi là một nhà phát triển có chuyên môn về  backend, frontend và DevOps.",
 
 		"contact.initializing": "Đang khởi tạo...",
 		"contact.ready": "Sẵn sàng kết nối.",
